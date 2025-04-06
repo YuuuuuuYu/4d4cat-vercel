452273a1a854209eaf9b88e3a090840cfd00c84c
yuuuuuuyu <yuitfhk@gmail.com>
2025-04-07 06:40:48 +0900

    Initial commit

---
+++ app/page.jsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      {/* 페이지 내용의 앞부분이 여기에 있어야 합니다 */}
+      
+      <footer>
+        
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
+    </div>
+  );
+}
