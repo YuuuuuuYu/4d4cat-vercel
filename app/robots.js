31be44e5ab4ff9e61d575d0f0e2abd5354de9983
yuuuuuuyu <yuitfhk@gmail.com>
2025-06-24 08:21:47 +0900

    chore: robots 내용 수정

--- app/robots.js
+++ app/robots.js
@@ -1,10 +1,18 @@
 export default function robots() {
     return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: '/private/',
-        },
+        rules: [
+            {
+                userAgent: '*',
+                allow: '/',
+                disallow: '/private/',
+            },
+            {
+                userAgent: 'Googlebot',
+                allow: '/',
+                disallow: '/private/',
+            }
+        ],
         sitemap: 'https://4d4cat.site/sitemap.xml',
+        host: 'https://4d4cat.site',
     }
 }
